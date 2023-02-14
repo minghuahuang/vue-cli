cee33cfce6fd4f3ee0f14d69e6c9374dde424c5b
huangminghua <huangminghua0615@163.com>
2023-02-14 11:12:13 +0800

    合并优化配置

---
+++ README.md
@@ -0,0 +1,23 @@
+# vue-cli
+webpack5 配置vue-cli项目
+
+## 项目启动
+
+安装依赖
+```shell
+yarn add
+```
+
+开发环境
+```shell
+yarn dev
+```
+
+生产环境
+```shell
+yarn build
+```
+
+## 项目技术栈
+
+webpack5 + vue3 实现 vue-cli项目。
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="home">
+		<h1>{{ title }}</h1>
+		<el-button>按钮</el-button>
+	</div>
+</template>
+
+<script setup>
+	import { ref } from "vue";
+
+	const title = ref("vue");
+</script>
+
+<style lang="scss" scoped>
+	@import "./style.scss";
+</style>
